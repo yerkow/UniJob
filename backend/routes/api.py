421c8cab1603cf39e194ae8d30387779567459d9
xsd <vpnlat3000@gmail.com>
2025-05-15 14:59:29 +0500

    v 1.0.2 изменения в api.py

--- backend/routes/api.py
+++ backend/routes/api.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.get('/home')
-def sayHello():
-    return {
-        "message": 'Hello World'
-    }
+@app.get('/home', response_class=HTMLResponse)
+def serve_html():
+    with open(r'C:\Users\xsd\Desktop\UniJob-main\syte\resume\index.html', 'r', encoding='utf-8') as file:
+        html_content = file.read()
+    return html_content
