9b12d32df953ab3ccc417254287c78d3447f27a8
xsd <vpnlat3000@gmail.com>
2025-05-15 15:44:16 +0500

    v 1.0.3 исправил локальный путь

--- backend/routes/api.py
+++ backend/routes/api.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from pathlib import Path
 
 app = FastAPI()
 
 @app.get('/home', response_class=HTMLResponse)
 def serve_html():
-    with open(r'C:\Users\xsd\Desktop\UniJob-main\syte\resume\index.html', 'r', encoding='utf-8') as file:
+    # Получаем путь к index.html относительно текущего файла
+
+    html_path = Path(__file__).parent.parent.parent / 'syte' / 'resume' / 'index.html'
+    print(html_path)
+    with open(html_path, 'r', encoding='utf-8') as file:
         html_content = file.read()
     return html_content
