a947e1f37f879bac151cc344f19756ddfdb50c31
Rollan Sarsembaev <sarsembaevrollan05@gmail.com>
2025-05-22 14:48:38 +0500

    изменил reqiure

--- backend/database/models.py
+++ backend/database/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime, func
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
@@ -6,3 +6,10 @@ class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     email = Column(String, unique=True, index=True, nullable=True)
+    hashed_password = Column(String(255))
+    first_name = Column(String(255))
+    last_name = Column(String(255))
+    created_at = Column(DateTime, server_default=func.now())
+
+    # @classmethod
+    # def hash_password(cls, password:str)
